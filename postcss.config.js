09bd97410c867697e8474046033d56f483b91437
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-04-07 23:01:14 +0200

    fix: postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -3,24 +3,22 @@
  */
 
 module.exports = {
-    plugins:
-        process.env.NODE_ENV === 'production'
-            ? [
-                  'postcss-flexbugs-fixes',
-                  [
-                      'postcss-preset-env',
-                      {
-                          autoprefixer: {
-                              flexbox: 'no-2009',
-                          },
-                          stage: 3,
-                          features: {
-                              'custom-properties': false,
-                          },
-                      },
-                  ],
-              ]
-            : [
-                  // No transformations in development
-              ],
+    plugins: {
+        'postcss-preset-env': {
+            autoprefixer: {
+                flexbox: 'no-2009',
+            },
+            stage: 3,
+            features: {
+                'custom-properties': false,
+            },
+        },
+        cssnano:
+            process.env.NODE_ENV === 'production'
+                ? {
+                      preset: 'default',
+                      discardComments: { removeAll: true },
+                  }
+                : false,
+    },
 };
