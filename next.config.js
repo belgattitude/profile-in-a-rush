b38adcf3c8203a9e9c42f28ba133fe7d391d820c
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-08-14 10:37:29 +0200

    feat: webpack5

--- next.config.js
+++ next.config.js
@@ -1,43 +1,9 @@
 const withMDX = require('@next/mdx')();
-const mdxImagesPlugin = require('remark-images');
-const mdxEmjoiPlugin = require('remark-emoji');
 const withBundleAnalyzer = require('@next/bundle-analyzer');
-const withFonts = require('next-fonts');
-const config = withMDX(
-  withFonts({
-    poweredByHeader: true,
-    reactStrictMode: true,
-    mdPlugins: [mdxImagesPlugin, mdxEmjoiPlugin],
-
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            limit: 100000,
-            name: '[name].[ext]',
-          },
-        },
-      });
-      /*
-        config.module.rules.push({
-            test: /\.(eot|ttf|woff|woff2)$/,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    limit: 100000,
-                    //name: '[name].[ext]',
-                    //publicPath: '/fonts',
-                    //outputPath: 'fonts',
-                },
-            },
-        });
-*/
-      return config;
-    },
-  })
-);
+const config = withMDX({
+  poweredByHeader: true,
+  reactStrictMode: true,
+});
 
 if (process.env.ANALYZE === 'true') {
   module.exports = withBundleAnalyzer(config);
