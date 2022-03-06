3e24fb8b34a227fbac8007e66804d02bc5a1e77f
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-03-07 00:37:12 +0100

    prep redis cache

--- src/pages/api/github/projects.ts
+++ src/pages/api/github/projects.ts
@@ -1,5 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getGithubRepos } from '../../../backend/github/getGithubRepos';
+import { getGithubRepos } from '@/backend/github/getGithubRepos';
+import { appCache } from '@/config/app-cache.config';
+
+async function getViewerGithubRepos() {
+  const data = await getGithubRepos();
+  const repos = data.viewer.repositories.nodes;
+  return repos.map((repo) => {
+    return {
+      name: repo.name,
+      description: repo.description,
+      homepageUrl: repo.homepageUrl,
+      stargazerCount: repo.stargazerCount,
+      primaryLanguage: repo.primaryLanguage?.name,
+      forkCount: repo.forkCount,
+      url: repo.projectsUrl,
+      createdAt: repo.createdAt,
+      updatedAt: repo.updatedAt,
+      isFork: repo.isFork,
+      visibility: repo.visibility,
+    };
+  });
+}
 
 export default async function apiGithubProjectsRoute(
   req: NextApiRequest,
@@ -9,24 +30,15 @@ export default async function apiGithubProjectsRoute(
     res.status(405).end(`Method ${req.method} Not Allowed`);
     return;
   }
-  const data = await getGithubRepos();
-  const repos = data.viewer.repositories.nodes;
-  const body = repos.map((repo) => {
-    return {
-      name: repo.name,
-      description: repo.description,
-      homepageUrl: repo.homepageUrl,
-      stargazerCount: repo.stargazerCount,
-      primaryLanguage: repo.primaryLanguage?.name,
+  const { _cache = null } = req.query;
+  const data = await appCache.getOrSet(
+    'getViewerGithubRepos',
+    getViewerGithubRepos,
+    {
+      ttl: 3_600,
+      disableCache: _cache === 'app-cache:off',
+    }
+  );
 
-      forkCount: repo.forkCount,
-      url: repo.projectsUrl,
-      createdAt: repo.createdAt,
-      updatedAt: repo.updatedAt,
-      isFork: repo.isFork,
-      visibility: repo.visibility,
-    };
-  });
-
-  res.status(200).send(JSON.stringify(body, null, 2));
+  res.status(200).send(JSON.stringify(data, null, 2));
 }
