efe3877fd81c75be6e80fd8ef44b492ca5b53b41
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-03-06 18:29:01 +0100

    chore: lint-staged/husky update

--- next.config.js
+++ next.config.js
@@ -9,7 +9,32 @@ const config = withTM(
   withMDX({
     poweredByHeader: true,
     reactStrictMode: true,
-    webpack5: true,
+    optimizeFonts: true,
+    // @link https://nextjs.org/docs/advanced-features/compiler#minification
+    swcMinify: true,
+    experimental: {
+      // React 18
+      // @link https://nextjs.org/docs/advanced-features/react-18
+      reactRoot: false,
+      // React 18 streaming
+      // @link https://nextjs.org/docs/advanced-features/react-18/streaming
+      runtime: undefined,
+      // React 18 server components
+      // @link https://nextjs.org/docs/advanced-features/react-18/server-components
+      serverComponents: false,
+      // Standalone build
+      // @link https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files-experimental
+      outputStandalone: true,
+      // Prefer loading of ES Modules over CommonJS
+      // @link {https://nextjs.org/blog/next-11-1#es-modules-support|Blog 11.1.0}
+      // @link {https://github.com/vercel/next.js/discussions/27876|Discussion}
+      esmExternals: true,
+      // Experimental monorepo support
+      // @link {https://github.com/vercel/next.js/pull/22867|Original PR}
+      // @link {https://github.com/vercel/next.js/discussions/26420|Discussion}
+      externalDir: false,
+    },
+
     webpack: (config) => {
       return config;
     },
